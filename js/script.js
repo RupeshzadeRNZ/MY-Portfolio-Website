eb9f2ea646a5f88e5885635590130f0fa7ef860e
Rupesh Zade <rupeshzade490@gmail>
2025-11-27 22:01:40 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Set Year
+  const year = document.getElementById("year");
+  if (year) year.textContent = new Date().getFullYear();
+
+  const navLinks = document.querySelectorAll(".nav-links a");
+  const sections = document.querySelectorAll("section[id]");
+
+  // Smooth Scroll (Click)
+  navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute("href"));
+      const offset = target.offsetTop;
+
+      window.scrollTo({
+        top: offset - 30,
+        behavior: "smooth"
+      });
+
+      // set active manually
+      navLinks.forEach(l => l.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+
+  // Scroll Spy (best version)
+  function activateMenuOnScroll() {
+    let scrollPos = window.scrollY + window.innerHeight / 3;
+
+    sections.forEach(section => {
+      const top = section.offsetTop;
+      const height = section.offsetHeight;
+
+      if (scrollPos >= top && scrollPos < top + height) {
+        const id = section.getAttribute("id");
+
+        navLinks.forEach(link => {
+          link.classList.remove("active");
+
+          if (link.getAttribute("href") === `#${id}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  }
+
+  window.addEventListener("scroll", activateMenuOnScroll);
+  activateMenuOnScroll(); // Run on load
+});
+
+// timeline 
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".timeline-item");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, {
+        threshold: 0.4
+    });
+
+    items.forEach(item => observer.observe(item));
+});
